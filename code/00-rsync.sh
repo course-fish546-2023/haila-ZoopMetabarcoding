d17759568e65333ed7ae3f2391e6dad441fddf2b
hailaschultz <103768032+hailaschultz@users.noreply.github.com>
2023-05-26 18:57:35 -0700

    Delete 00-rsync.sh

--- code/00-rsync.sh
+++
@@ -1,2 +0,0 @@
-#move project directory onto mox - type code below into terminal and sign in with password
-rsync -avz haila-ZoopMetabarcoding schulh2@mox.hyak.uw.edu:/gscratch/scrubbed/schulh2/assign_09
